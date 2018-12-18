77370e26535f6aa1bc46313d12765edf032534aa
Alban Pierson <pro.pierson.alban@gmail.com>
2018-12-18 23:02:44 +0100

    Refractor switching language selector
    Use session parameter [:language] istead of javascript because javascript in ruby view prevent page loading

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,20 +1,7 @@
 $(document).on('turbolinks:load', function(){
 
     $("[data-do='select_language']").change(function() {
-
-        var CurrentUrl=self.location.href;
-        var CurrentLang = CurrentUrl.split('\/')[3];
-        if (this.value === "en" && CurrentLang === "fr"){
-            //window.location.href = '/languages/' + this.value;
-            document.location.href = CurrentUrl.replace("\/"+CurrentLang+"\/","/");
-        }
-        else{
-            if(this.value === "fr" && CurrentLang.length != null && CurrentLang !== "fr"){
-                //window.location.href = '/languages/' + this.value;
-                document.location.href = '/' + this.value  + window.location.pathname;
-            }
-        }
-
+        window.location.href = '/languages/' + this.value
     })
 
 });
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     devise_for :users
 
     root to: 'announces#index'
+    resources :languages, only: :show
     resources :announces
 
 
