2bd5ca1e0195b7faf100425a7f89b3c0b2fa25b1
Alban Pierson <pro.pierson.alban@gmail.com>
2018-12-19 21:37:15 +0100

    Rename announces into listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,12 @@
 class ListingsController < ApplicationController
 
   def index
+    @announces = Annonce.all
+  end
+
+  def show
+    @announce = Annonce.find(params[:id])
+    #@annonces = PostDecorator.new(post).to_post
   end
 
 end
