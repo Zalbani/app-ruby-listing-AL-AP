8028c5df1ed7d8f42964a29e0feacf025cfc3e93
Alban Pierson <pro.pierson.alban@gmail.com>
2019-01-30 12:26:36 +0100

    Add form for create a new listing / and translated

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,7 +11,28 @@ class ListingsController < ApplicationController
   end
 
   def new
+    @listing = Annonce.new
+  end
 
+  def create
+    @listing = Annonce.new(listings_params)
+    if @listing.save
+      redirect_to root_path
+    else
+      redirect_to new_listing_path
+    end
+  end
+
+  private
+
+  def listings_params
+    params.require(:listing).permit(
+        :category_id,
+        :content,
+        :price,
+        :title,
+        :picture,
+        ).merge(user_id: current_user.id)
   end
 
 end
