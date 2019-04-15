8a7de4fdd2ea5695fa742487f0c99d33351c7072
AlexisLauradoux <alexis.lauradoux@ynov.com>
2019-04-15 12:11:14 +0200

    Edit user

--- app/controllers/account/user_controller.rb
+++ app/controllers/account/user_controller.rb
@@ -1,11 +1,24 @@
 class Account::UserController < ApplicationController
 
   def index
-
+    @user = current_user
   end
 
-  def edit
+  def create
+    @user = current_user
+    current_user.update(user_params)
+    redirect_to account_user_index_path
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(
+     :first_name,
+     :last_name,
+     :email,
+     :password,
+     :password_confirmation
+    )
   end
 
 end
