aef4d74ac6d67304512e0d5f1cdfd9ebb0a050e3
Alban Pierson <pro.pierson.alban@gmail.com>
2019-04-16 10:32:59 +0200

    Test generate token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :annonces
   belongs_to :role
 
   validates :role_id,presence: true
+
+  #validates :auth_token, uniqueness: true
+  #before_create generate_token
+
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+
+  def generate_token
+    self.auth_token = SecureRandom.uuid
+    if self.save
+      return auth_token
+    else
+      return nil
+    end
+  end
 
 end
--- test/integration/api/v1/auth_test.rb
+++ test/integration/api/v1/auth_test.rb
@@ -5,18 +5,21 @@ feature "Auth" do
   before  do
     @user = User.create(
                     email: 'email@domaine.com',
+                    first_name: 'Prenom',
+                    last_name: 'Nom',
                     password: 'password123',
-                    password_confirmation: 'password123'
+                    password_confirmation: 'password123',
+                    role: roles(:admin)
     )
   end
 
+  focus
   it "Return a token if i'm logged in" do
-    post api_v1_auth_path,{
+    post api_v1_auth_path, {
         email: @user.email,
-        password:@user.password
+        password: 'password123'
     }
-
-    assert_equal 200 , last_response.status.must_equal
+    assert_equal 200 , last_response.status
   end
 
 end
