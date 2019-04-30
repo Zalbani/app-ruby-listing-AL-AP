f9f2ac225a3fc0fc08f7efeba43dcaca39357c8d
Alban Pierson <pro.pierson.alban@gmail.com>
2019-04-30 09:59:10 +0200

    new try

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,7 +1,8 @@
 class Api::ApiController < ApplicationController
 
-  #before_action :auth_with_token, except: :sign_in
-  #skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+  before_action :auth_with_token, except: :sign_in
+
 
 
 
