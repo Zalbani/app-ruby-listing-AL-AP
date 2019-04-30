b7697cc13db3415c4f481fb6296e90efb45955f4
Alban Pierson <pro.pierson.alban@gmail.com>
2019-04-30 14:33:27 +0200

    Update api controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,6 +1,7 @@
 class Api::ApiController < ApplicationController
 
   skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
   #before_action :auth_with_token, except: :sign_in
 
 
