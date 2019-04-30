b018854184c0e13d8b1e1329f92dd49be4da13ce
Alban Pierson <pro.pierson.alban@gmail.com>
2019-04-30 14:31:30 +0200

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_language
-  protect_from_forgery with: :exception
+  protect_from_forgery unless: -> { request.format.json? }
 
   def set_language
     if !session[:language]
